9cc822c44958fa9c95a22932b9ab8d0f3e2e38a0
Vano Qochladze <vkochladze@gmail.com>
2023-12-07 15:52:41 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+//1
+console.log("1");
+
+let myVar = 10;
+
+if (myVar < 50) {
+  console.log("<50");
+} else if (myVar > 50) {
+  console.log(">20");
+} else {
+  ("error)");
+}
+
+//2
+console.log("2");
+
+let fname = "mariam";
+
+if (fname == "mariam") {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+//3
+console.log("3");
+
+switch (fname) {
+  case "mariam":
+    console.log("true");
+    break;
+
+  default:
+    console.log("false");
+    break;
+}
+
+//4
+console.log("4");
+console.log(typeof fname);
+
+//5
+console.log("5");
+var number = 10;
+
+var str = number.toString();
+
+console.log(typeof str);
